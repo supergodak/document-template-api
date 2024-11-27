20631bc897bf347d57f0ba622f9079fbc1aa86ee
Hideyuki Takeya <supergodak@gmail.com>
2024-11-27 14:00:15 +0900

    Netlify,ローカルの両方での動作確認対応

--- functions/api.py
+++ functions/api.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from mangum import Mangum  # Netlify対応用
+from mangum import Mangum  # Netlify Functions対応用
 
 app = FastAPI()
 
@@ -16,5 +16,11 @@ async def read_root():
 async def replace_text(data: InputData):
     return {"replaced": data.value.replace("{key}", data.key)}
 
+# Netlify Functions対応用ハンドラ
 handler = Mangum(app)
+
+# ローカル実行用
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
 
